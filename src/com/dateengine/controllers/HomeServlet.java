69d12bf87dedd52eb11e1250c995bf2d2374d77d
Ikai Lan <ikai.lan@gmail.com>
2009-10-20 11:07:16 -0700

    Initial commit

---
+++ src/com/dateengine/controllers/HomeServlet.java
@@ -0,0 +1,18 @@
+package com.dateengine.controllers;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import java.io.IOException;
+
+public class HomeServlet extends HttpServlet {
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/templates/home.jsp");
+
+      dispatcher.forward(request, response);
+
+   }
+}
