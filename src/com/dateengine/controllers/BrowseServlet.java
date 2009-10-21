88bd1a3faf97741f18289b55a3553bb84c6d0fab
Ikai Lan <ikai.lan@gmail.com>
2009-10-21 12:46:20 -0700

    Adding profile browsing

--- src/com/dateengine/controllers/BrowseServlet.java
+++ src/com/dateengine/controllers/BrowseServlet.java
@@ -1,15 +1,34 @@
 package com.dateengine.controllers;
 
+import com.dateengine.PMF;
+import com.dateengine.Profile;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
+import javax.jdo.Query;
+import javax.jdo.PersistenceManager;
 import java.io.IOException;
+import java.util.List;
 
 public class BrowseServlet extends HttpServlet {
 
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      PersistenceManager pm   = PMF.get().getPersistenceManager();
+      Query query             = pm.newQuery(Profile.class);
+
+      List<Profile> profiles;
+
+      try {
+         profiles = (List<Profile>) query.execute();            
+      } finally {
+         query.closeAll();
+      }
+
+      request.setAttribute("profiles", profiles);
+
       RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/templates/browse/list.jsp");
       dispatcher.forward(request, response);
 
--- web/WEB-INF/templates/browse/list.jsp
+++ web/WEB-INF/templates/browse/list.jsp
@@ -1,11 +1,23 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false" %>
+<%@ taglib uri='http://java.sun.com/jsp/jstl/core' prefix='c' %>
+
 <html>
   <head><title>Browse Profiles</title></head>
   <body>
     <h1>Profile Browse</h1>
     <%@ include file="../shared/navigation.jsp" %>
     <div class="profiles">
-              
+        <ul>
+        <c:forEach var="profile" items="${profiles}">
+            <li class="mini-profile">
+                <h3>${profile.username}</h3>
+                <p>
+                    ${profile.aboutMe}
+                </p>
+            </li>
+        </c:forEach>
+        </ul>
     </div>
 
   </body>
